b31a1cffe581b385206460d6497a670caaeb9eea
Tommy Ly <hvl2108@columbia.edu>
2024-06-08 15:56:08 +0000

    commit firs tpart of pipeline

--- pipelines/run_pipeline.py
+++ pipelines/run_pipeline.py
@@ -18,19 +18,23 @@ from eks.utils import format_data, populate_output_dataframe
 def pipeline(config_file: str):
 
     # load pipeline config file
-    cfg = yaml.safe_load(open(config_file, "r"))
-    data_dir = cfg.get("data", {}).get("data_dir", "/tmp/data")
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
+    with open(config_file, "r") as file:
+        cfg = yaml.safe_load(file)
+        
+    # load lightning pose config file from the path specified in pipeline config
+    lightning_pose_config_path = cfg.get("lightning_pose_config")
+    with open(lightning_pose_config_path, "r") as file:
+        lightning_pose_cfg = yaml.safe_load(file)
+    
+    cfg_lp = DictConfig(lightning_pose_cfg)
+    data_dir = cfg_lp.data.data_dir
+    
     # -------------------------------------------------------------------------------------
     # train k supervised models on n hand-labeled frames and compute labeled OOD metrics
     # -------------------------------------------------------------------------------------
     print(f'training {len(cfg["init_ensemble_seeds"])} baseline models')
     for k in cfg["init_ensemble_seeds"]:
-        # load lightning pose config file
-        cfg_lp = DictConfig(cfg["lightning_pose_config"])
-
-        # update data.data_dir, maybe some other paths
+        
         cfg_lp.data.data_dir = data_dir
         # update training seeds
         cfg_lp.training.rng_seed_data_pt = k
@@ -133,43 +137,43 @@ def pipeline(config_file: str):
     # ''' Output from EKS can be csv or DataFrame, whatever is easier for the next step '''
 
 
-    # -------------------------------------------------------------------------------------
-    # select frames to add to the dataset
-    # -------------------------------------------------------------------------------------
-    print(
-        f'selecting {cfg["n_pseudo_labels"]} pseudo-labels using {cfg["pseudo_labeler"]} '
-        f'({cfg["selection_strategy"]} strategy)'
-    )
-    # - we need to add frames to the existing dataset
-    # - for each strategy/run/whatever, need to make a new csv file with updated frames
+    # # -------------------------------------------------------------------------------------
+    # # select frames to add to the dataset
+    # # -------------------------------------------------------------------------------------
+    # print(
+    #     f'selecting {cfg["n_pseudo_labels"]} pseudo-labels using {cfg["pseudo_labeler"]} '
+    #     f'({cfg["selection_strategy"]} strategy)'
+    # )
+    # # - we need to add frames to the existing dataset
+    # # - for each strategy/run/whatever, need to make a new csv file with updated frames
 
-    # train model(s) on expanded dataset
-    print(f'training {len(cfg["final_ensemble_seeds"])} baseline models')
-    for k in cfg["final_ensemble_seeds"]:
-        # load lightning pose config file
-        cfg_lp = use omega conf to load
-        # - update data.data_dir, maybe some other paths
-        # - update training.rng_seed_data_pt
-        # - add iteration-specific fields to the config
-        # - NEW: change labeled data field (data.csv_file)
+    # # train model(s) on expanded dataset
+    # print(f'training {len(cfg["final_ensemble_seeds"])} baseline models')
+    # for k in cfg["final_ensemble_seeds"]:
+    #     # load lightning pose config file
+    #     cfg_lp = use omega conf to load
+    #     # - update data.data_dir, maybe some other paths
+    #     # - update training.rng_seed_data_pt
+    #     # - add iteration-specific fields to the config
+    #     # - NEW: change labeled data field (data.csv_file)
 
-        # define the output directory
-        results_dir = todo
+    #     # define the output directory
+    #     results_dir = todo
 
-        # train model
-        # if we run inference on videos inside train(), then we should pass a list of video
-        # directories to loop over; these should probably be stored in pipeline config file
-        train(cfg=cfg_lp, results_dir=results_dir)
+    #     # train model
+    #     # if we run inference on videos inside train(), then we should pass a list of video
+    #     # directories to loop over; these should probably be stored in pipeline config file
+    #     train(cfg=cfg_lp, results_dir=results_dir)
 
-    # -------------------------------------------------------------------------------------
-    # run inference on all InD/OOD videos and compute unsupervised metrics
-    # -------------------------------------------------------------------------------------
-    # do as above
+    # # -------------------------------------------------------------------------------------
+    # # run inference on all InD/OOD videos and compute unsupervised metrics
+    # # -------------------------------------------------------------------------------------
+    # # do as above
 
-    # -------------------------------------------------------------------------------------
-    # save out all predictions/metrics in dataframe(s)
-    # -------------------------------------------------------------------------------------
-    # can think about this later
+    # # -------------------------------------------------------------------------------------
+    # # save out all predictions/metrics in dataframe(s)
+    # # -------------------------------------------------------------------------------------
+    # # can think about this later
 
 
 if __name__ == "__main__":
