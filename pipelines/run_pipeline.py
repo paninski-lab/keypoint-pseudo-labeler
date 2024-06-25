f2ab826f1eefffb3ca8ada2def52b59e4cfb90f9
Keemin Lee <keemin7@gmail.com>
2024-06-25 20:09:00 +0000

    directory updates for train/infer and eks

--- pipelines/run_pipeline.py
+++ pipelines/run_pipeline.py
@@ -30,13 +30,13 @@ def pipeline(config_file: str):
     
     cfg_lp = DictConfig(lightning_pose_cfg)
     data_dir = cfg_lp.data.data_dir
+    print(data_dir)
     
     # -------------------------------------------------------------------------------------
     # train k supervised models on n hand-labeled frames and compute labeled OOD metrics
     # -------------------------------------------------------------------------------------
-    print(f'training {len(cfg["init_ensemble_seeds"])} baseline models')
-    eks_input_csv_names = []  # save for EKS
-    for k in cfg["init_ensemble_seeds"]:
+    print(f'training {len(cfg["ensemble_seeds"])} baseline models')
+    for k in cfg["ensemble_seeds"]:
         
         cfg_lp.data.data_dir = data_dir
         
@@ -51,15 +51,19 @@ def pipeline(config_file: str):
         
         script_dir = os.path.dirname(os.path.abspath(__file__))
         parent_dir = os.path.dirname(script_dir)
-        results_dir = os.path.join(parent_dir, f"../outputs/mirror-mouse/100_1000-eks-random/rng{k}")
+        results_dir = os.path.join(parent_dir, f"../outputs/{os.path.basename(data_dir)}/hand={cfg_lp.training.train_frames}_pseudo={cfg['n_pseudo_labels']}/networks/rng{k}")
         os.makedirs(results_dir, exist_ok=True)
         
         """
-        mirror-mouse/100_1000-eks-random/rng0
-        mirror-mouse/100_1000-eks-random/rng1
-        mirror-mouse/100_1000-eks-random/eks
-        ...
-        mirror-mouse/100_10000-eks-strategy2/rng0
+        Directory layout:
+        outputs/{dataset}/hand=100_pseudo=1000/
+	        networks/
+                rng{k}
+	        post-processors/
+                {pseudo-labeler}_rng={k[0]}-{k[-1]}
+	        selected-frames/
+                hand=100_rng={k[hand-label_seed]}_pseudo=1000_{pseudo-labeler}_{strategy}_rng={k[0]}-{k[-1]} 
+        Where k is based on ensemble_seeds in .yaml config
         """
 
         model_config_checked = os.path.join(results_dir, "config.yaml")
@@ -93,9 +97,6 @@ def pipeline(config_file: str):
                 num_videos += len(video_files)
                 for video_file in video_files:
                     csv_name = video_file.replace(".mp4", ".csv")
-                    if csv_name not in eks_input_csv_names:
-                        print(f'Appending: {csv_name} to EKS input csv list')
-                        eks_input_csv_names.append(csv_name)
                     results_df = inference_with_metrics(
                         video_file=os.path.join(data_dir, video_dir, video_file),
                         cfg=cfg_lp,
@@ -114,19 +115,29 @@ def pipeline(config_file: str):
     print("Starting EKS")
     script_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(script_dir)
-    results_dir = os.path.join(parent_dir, f"../outputs/mirror-mouse/100_1000-eks-random")
-    input_dir = results_dir
-    os.makedirs(results_dir, exist_ok=True)  # can be removed later (if results_dir exists)
-    eks_dir = os.path.join(results_dir, "eks")
-    os.makedirs(eks_dir, exist_ok=True)  # Ensure eks directory exists
+    input_dir = os.path.join(parent_dir, 
+    f"../outputs/{os.path.basename(data_dir)}/hand={cfg_lp.training.train_frames}_pseudo={cfg['n_pseudo_labels']}/networks/")
+    results_dir = os.path.join(parent_dir, 
+    f"../outputs/{os.path.basename(data_dir)}/hand={cfg_lp.training.train_frames}_pseudo={cfg['n_pseudo_labels']}/post-processors/{cfg['pseudo_labeler']}_rng={cfg['ensemble_seeds'][0]}-{cfg['ensemble_seeds'][-1]}")
+    os.makedirs(results_dir, exist_ok=True)
     data_type = cfg["data_type"]
     output_df = None
+
+    # Collect input csv names from video directory
+    input_csv_names = []
+    for video_dir in cfg["video_directories"]:
+        video_files = os.listdir(os.path.join(data_dir, video_dir))
+        for video_file in video_files:
+            csv_name = video_file.replace(".mp4", ".csv")
+            if csv_name not in input_csv_names:
+                print(f'Appending: {csv_name} to post-processing input csv list')
+                input_csv_names.append(csv_name)
 
     if cfg["pseudo_labeler"] == "eks":
         bodypart_list = cfg_lp["data"]["keypoint_names"]
         s = None  # optimize s
         s_frames = [(None, None)] # use all frames for optimization
-        for csv_name in eks_input_csv_names:
+        for csv_name in input_csv_names:
             # Load and format input files and prepare an empty DataFrame for output.
             input_dfs, output_df, _ = format_data_walk(input_dir, data_type, csv_name)
             print(f'Found {len(input_dfs)} input dfs')
@@ -150,7 +161,7 @@ def pipeline(config_file: str):
             markers_3d_array = markers_3d_array[:, :, key_cols]
 
             # Call the smoother function
-            df_dicts, s_finals, nll_values_array = ensemble_kalman_smoother_singlecam(
+            df_dicts, s_finals = ensemble_kalman_smoother_singlecam(
                 markers_3d_array,
                 bodypart_list,
                 s,
@@ -164,7 +175,7 @@ def pipeline(config_file: str):
             for k in range(len(bodypart_list)):
                 df = df_dicts[k][bodypart_list[k] + '_df']
                 output_df = populate_output_dataframe(df, bodypart_list[k], output_df)
-                output_path = os.path.join(eks_dir, csv_name)
+                output_path = os.path.join(results_dir, csv_name)
                 output_df.to_csv(output_path)
 
             print(f"EKS DataFrame output for {csv_name} successfully converted to CSV. See at {output_path}")
