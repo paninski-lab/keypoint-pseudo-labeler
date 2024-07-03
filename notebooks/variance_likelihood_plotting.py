e3fae03670bf7d6e48adbee0bb5bb10d32a81997
Keemin Lee <keemin7@gmail.com>
2024-07-03 20:25:57 +0000

    no longer references configs to plot

--- notebooks/variance_likelihood_plotting.py
+++ notebooks/variance_likelihood_plotting.py
@@ -1,7 +1,5 @@
 import os
-import yaml
-from omegaconf import DictConfig
-
+import argparse
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,33 +10,20 @@ from matplotlib.ticker import LogLocator
 from pseudo_labeler.utils import format_data_walk
 from pseudo_labeler.evaluation import compute_likelihoods_and_variance, plot_heatmaps
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Pipeline example script")
+    parser.add_argument('--dir', type=str, required=True, help="Directory containing the input data")
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    config_file = "./configs/pipeline_example.yaml"
-    with open(config_file, "r") as file:
-        cfg = yaml.safe_load(file)
+    args = parse_args()
 
-    lightning_pose_config_path = cfg.get("lightning_pose_config")
-    with open(lightning_pose_config_path, "r") as file:
-        lightning_pose_cfg = yaml.safe_load(file)
-
-    cfg_lp = DictConfig(lightning_pose_cfg)
-
-    data_dir = cfg_lp.data.data_dir
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    parent_dir = os.path.dirname(script_dir)
-    input_dir = os.path.join(parent_dir, (
-                f"../outputs/{os.path.basename(data_dir)}/hand={cfg_lp.training.train_frames}_"
-                f"pseudo={cfg['n_pseudo_labels']}/networks/"
-            )
-        )
+    input_dir = args.dir
     data_type = 'lp'
     csv_name = "predictions_new.csv"
 
-    bodypart_list = cfg_lp["data"]["keypoint_names"]
-
     input_dfs, output_df, _ = format_data_walk(input_dir, data_type, csv_name)
     print(f'Found {len(input_dfs)} models.')
-
-    likelihoods_above_thresh, summed_ensemble_vars, combined_df = compute_likelihoods_and_variance(cfg_lp, input_dfs, bodypart_list)
-    plot_heatmaps(likelihoods_above_thresh, summed_ensemble_vars, bodypart_list, data_dir, script_dir)
+    likelihood_thresh = 0.9
+    likelihoods_above_thresh, summed_ensemble_vars, combined_df, bodypart_list = compute_likelihoods_and_variance(input_dfs, likelihood_thresh)
+    plot_heatmaps(likelihoods_above_thresh, summed_ensemble_vars, bodypart_list, input_dir, likelihood_thresh)
